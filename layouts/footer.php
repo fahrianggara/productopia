31f906384b536cf4b0492d1f4cb4e8bb50f8b41e
zexpher <fahriangga30@gmail.com>
2023-06-01 17:46:21 +0700

    Penambahan file agar dinamis

---
+++ layouts/footer.php
@@ -0,0 +1,39 @@
+<footer>
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h3>Tentang</h3>
+                        <p>
+                            Productopia adalah sebuah website yang menyediakan berbagai
+                            macam produk yang dapat dibeli secara online.
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Menu</h3>
+                        <ul class="mb-3">
+                            <li><a class="underline active" href="javascript:void(0)">Utama</a></li>
+                            <li><a class="underline" href="javascript:void(0)">Produk</a></li>
+                            <li><a class="underline" href="javascript:void(0)">Pencarian</a></li>
+                            <li><a class="underline" href="javascript:void(0)">Tentang Kami</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Hubungi Kami</h3>
+                        <ul>
+                            <li><a class="underline" href="javascript:void(0)">Facebook</a></li>
+                            <li><a class="underline" href="javascript:void(0)">Instagram</a></li>
+                            <li><a class="underline" href="javascript:void(0)">Twitter</a></li>
+                            <li><a class="underline" href="javascript:void(0)">WhatsApp</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <p>&copy; 2021 Productopia. All Rights Reserved.</p>
+            </div>
+        </div>
+        <div></div>
+    </footer>
