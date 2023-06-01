ea556ef26791822b57025dd283bb42e15012628e
ilhamrams2 <ilhamramadangm32@gmail.com>
2023-06-01 17:58:54 +0700

    Udah Jon

--- index.php
+++ index.php
@@ -1,21 +1,60 @@
-<section id="welcome">
-    <div class="container">
-        <div class="row flex-row-reverse align-items-center">
-            <div class="welcome-banner col-xl-6 col-lg-6 col-md-6">
-                <picture class="welcome-banner-inner">
-                    <img src="assets/images/banner.svg">
-                </picture>
-            </div>
-            <div class="col-xl-6 col-lg-6 col-md-6">
-                <div class="welcome-text">
-                    <h1>Selamat Datang di <u>Productopia</u></h1>
-                    <p>
-                        Productopia adalah sebuah website yang menyediakan berbagai macam produk yang dapat
-                        dibeli secara online.
-                    </p>
-                    <a href="#elektronik" class="btn welcome-btn">Lihat Produk</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productopia</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap4/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.css">
+    <link rel="stylesheet" href="assets/plugins/tinyslider/tiny-slider.css">
+    <link rel="stylesheet" href="assets/css/b.css">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include('layouts/navbar.php'); ?>
+
+    <main id="main">
+        <?php
+            if (isset($_GET['page'])) {
+                $page = $_GET['page'];
+
+                switch ($page) {
+                    case 'home':
+                        include('pages/homepage.php');
+                        break;
+                    case 'product':
+                        include('pages/product.php');
+                        break;
+                    case 'search':
+                        include('pages/search.php');
+                        break;
+                    case 'about':
+                        include('pages/about.php');
+                        break;
+                    default:
+                        include('pages/homepage.php');
+                        break;
+                }
+            } else {
+                include('layouts/welcome.php');
+            }
+        ?>
+    </main>
+
+    <!-- Footer -->
+    <?php include('layouts/footer.php'); ?>
+
+    <!-- Javascript -->
+    <script src="assets/plugins/jquery/jquery.min.js"></script>
+    <script src="assets/plugins/bootstrap4/bootstrap.bundle.min.js"></script>
+    <script src="assets/plugins/jquery/sticky/jquery.sticky.js"></script>
+    <script src="assets/plugins/jquery/easing/jquery.easing.min.js"></script>
+    <script src="assets/plugins/tinyslider/tiny-slider.js"></script>
+    <script src="assets/js/a.js"></script>
+</body>
+
---
+++ layouts/welcome.php
@@ -0,0 +1,22 @@
+<!-- layout.php -->
+<section id="welcome">
+    <div class="container">
+        <div class="row flex-row-reverse align-items-center">
+            <div class="welcome-banner col-xl-6 col-lg-6 col-md-6">
+                <picture class="welcome-banner-inner">
+                    <img src="assets/images/banner.svg">
+                </picture>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6">
+                <div class="welcome-text">
+                    <h1>Selamat Datang di <u>Productopia</u></h1>
+                    <p>
+                        Productopia adalah sebuah website yang menyediakan berbagai macam produk yang dapat
+                        dibeli secara online.
+                    </p>
+                    <a href="#elektronik" class="btn welcome-btn">Lihat Produk</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
