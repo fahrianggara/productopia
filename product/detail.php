044a24018a04e21daf2a9d5f9e37622abad38e91
zexpher <fahriangga30@gmail.com>
2023-06-11 14:56:50 +0700

    Validasi jika data tidak ditemukan maka lempar ke halaman product

--- product/detail.php
+++ product/detail.php
@@ -1,158 +1,153 @@
 <?php
-$category;
-$id = $_GET['id'];
+    $category; // Deklarasi variabel category
+    $id = $_GET['id']; // Ambil id dari url
+    $found = false; // Set found menjadi false
 
-foreach ($product as $index) {
-    if ($index['id'] == $id) {
-        $showData = $index;
-        break;
-    }
-}
-?>
-
-<!-- Jika data ditemukan -->
-<?php if ($index) { ?>
-    <!-- Jika data tidak kosong  -->
-    <?php if (!empty($product)) {
-        // Looping isi data
-        foreach ($product as $row) {
-            // Jika id data sama dengan id yang dikirimkan
-            if ($row['id'] == $id) { ?>
-                <?php
-                    $category = $row['category'];
-
-                    function explodeItem($row, $key)
-                    {
-                        $exp = explode(',', $row[$key]);
-                        foreach ($exp as $e) {
-                            echo "<li><input type='radio' name='$key' value='$e'>$e</li>";
-                        }
-                    }
-                ?>
-
-                <section id="product-detail" style="padding-top: 40px;">
-                    <div class="container">
-                        <div class="row">
-
-                            <figure class="image col-xl-6 col-lg-6 col-md-6">
-                                <img src="<?= $row['image'] ?>">
-                            </figure>
-
-                            <article class="col-xl-6 col-lg-6 col-md-6">
-
-                                <p class="category">
-                                    <?php
-                                        if ($row['badge']) {
-                                            echo $row['badge'] . " •";
-                                        }
-                                    ?>
-
-                                    <?= $row['category'] ?>
-                                </p>
-
-                                <h1 class="name"><?= $row['name'] ?></h1>
-
-                                <div class="price <?= $row['description'] == "" ? "mb-4" : "" ?>">
-                                    <?php if ($row['discount'] == "") { ?>
-                                        <span class="original"><?= formatRupiah($row['original']) ?></span>
-                                    <?php } else { ?>
-                                        <span class="original"><?= formatRupiah($row['original']) ?></span>
-                                        <span class="discount"><?= formatRupiah($row['discount']) ?></span>
-                                    <?php } ?>
-                                </div>
-
-                                <?php if ($row['description'] != "") { ?>
-                                    <hr class="rule-detail">
-
-                                    <div class="description">
-                                        <p><?= $row['description'] ?></p>
-                                    </div>
-                                <?php } ?>
-
-                                <div class="card">
-                                    <div class="card-header font-weight-bold">
-                                        Atur Pesanan
-                                    </div>
-                                    <form id="orderForm" action="?page=checkout" method="POST">
-                                        <input type="hidden" value="<?= $row['image'] ?>" name="image">
-                                        <input type="hidden" value="<?= $row['name'] ?>" name="name">
-                                        <input type="hidden" value="<?= $row['id'] ?>" name="id">
-                                        <input type="hidden" value="<?= $row['original'] ?>" name="original">
-                                        <input type="hidden" value="<?= $row['discount'] == null ? "" : $row['discount'] ?>" name="discount">
-
-                                        <div class="card-body section-bg">
-
-                                            <?php if ($row['size']) { ?>
-                                                <div class="form-group">
-                                                    <label>Pilih Ukuran: </label>
-                                                    <ul class="selection-input">
-                                                        <?php explodeItem($row, 'size'); ?>
-                                                    </ul>
-                                                </div>
-                                            <?php } ?>
-
-                                            <?php if ($row['color']) { ?>
-                                                <div class="form-group">
-                                                    <label>Pilih Warna: </label>
-                                                    <ul class="selection-input">
-                                                        <?php explodeItem($row, 'color'); ?>
-                                                    </ul>
-                                                </div>
-                                            <?php } ?>
-
-                                            <?php if ($row['weight']) { ?>
-                                                <div class="form-group">
-                                                    <label>Pilih Berat: </label>
-                                                    <ul class="selection-input">
-                                                        <?php explodeItem($row, 'weight'); ?>
-                                                    </ul>
-                                                </div>
-                                            <?php } ?>
-
-                                            <div class="form-group">
-                                                <label>Kuantitas: </label>
-
-                                                <div class="qty">
-                                                    <button type="button" class="min-qty" onclick="this.parentNode.querySelector('#quantity').stepDown()">
-                                                        <i class="fas fa-minus"></i>
-                                                    </button>
-                                                    <div class="form-outline">
-                                                        <input id="quantity" min="1" max="10" name="quantity" value="1" type="number" class="form-control" readonly />
-                                                    </div>
-                                                    <button type="button" class="plus-qty" onclick="this.parentNode.querySelector('#quantity').stepUp()">
-                                                        <i class="fas fa-plus"></i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="region">Wilayah Pengiriman: </label>
-                                                <select class="input-billing form-control" id="region" name="region" required>
-                                                    <option value="jabodetabek">Jabodetabek</option>
-                                                    <option value="jawa_barat">Jawa Barat</option>
-                                                    <option value="jawa_tengah">Jawa Tengah</option>
-                                                    <option value="jawa_timur">Jawa Timur</option>
-                                                    <option value="luar_jawa">Luar Jawa</option>
-                                                    <option value="luar_negeri">Luar Negeri</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer">
-                                            <button class="btn btn-sm btn-beli btn-primary">
-                                                Beli Sekarang
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-
-                            </article>
-                        </div>
-                    </div>
-                </section>
-            <?php
-            }
+    function explodeItem($row, $key)
+    {
+        $exp = explode(',', $row[$key]);
+        foreach ($exp as $e) {
+            echo "<li><input type='radio' name='$key' value='$e'>$e</li>";
         }
     }
-} ?>
+?>
+
+<?php if (!empty($product)) { // Jika data tidak kosong
+    foreach ($product as $row) { // Looping isi data
+        if ($row['id'] == $id) { ?> <!-- Jika id data sama dengan id yang dikirim -->
+            <?php
+                $found = true; // Set found menjadi true
+                $category = $row['category']; // Set category menjadi category dari data
+            ?>
+
+            <section id="product-detail" style="padding-top: 40px;">
+                <div class="container">
+                    <div class="row">
+
+                        <figure class="image col-xl-6 col-lg-6 col-md-6">
+                            <img src="<?= $row['image'] ?>">
+                        </figure>
+
+                        <article class="col-xl-6 col-lg-6 col-md-6">
+
+                            <p class="category">
+                                <?php
+                                    if ($row['badge']) { 
+                                        echo $row['badge'] . " •"; 
+                                    }
+                                ?>
+
+                                <?= $row['category'] ?>
+                            </p>
+
+                            <h1 class="name"><?= $row['name'] ?></h1>
+
+                            <div class="price <?= $row['description'] == "" ? "mb-4" : "" ?>">
+                                <?php if ($row['discount'] == "") { ?>
+                                    <span class="original"><?= formatRupiah($row['original']) ?></span>
+                                <?php } else { ?>
+                                    <span class="original"><?= formatRupiah($row['original']) ?></span>
+                                    <span class="discount"><?= formatRupiah($row['discount']) ?></span>
+                                <?php } ?>
+                            </div>
+
+                            <?php if ($row['description'] != "") { ?>
+                                <hr class="rule-detail">
+
+                                <div class="description">
+                                    <p><?= $row['description'] ?></p>
+                                </div>
+                            <?php } ?>
+
+                            <div class="card">
+                                <div class="card-header font-weight-bold">
+                                    Atur Pesanan
+                                </div>
+                                <form id="orderForm" action="?page=checkout" method="POST">
+                                    <input type="hidden" value="<?= $row['image'] ?>" name="image">
+                                    <input type="hidden" value="<?= $row['name'] ?>" name="name">
+                                    <input type="hidden" value="<?= $row['id'] ?>" name="id">
+                                    <input type="hidden" value="<?= $row['original'] ?>" name="original">
+                                    <input type="hidden" value="<?= $row['discount'] == null ? "" : $row['discount'] ?>" name="discount">
+
+                                    <div class="card-body section-bg">
+
+                                        <?php if ($row['size']) { ?>
+                                            <div class="form-group">
+                                                <label>Pilih Ukuran: </label>
+                                                <ul class="selection-input">
+                                                    <?php explodeItem($row, 'size'); ?>
+                                                </ul>
+                                            </div>
+                                        <?php } ?>
+
+                                        <?php if ($row['color']) { ?>
+                                            <div class="form-group">
+                                                <label>Pilih Warna: </label>
+                                                <ul class="selection-input">
+                                                    <?php explodeItem($row, 'color'); ?>
+                                                </ul>
+                                            </div>
+                                        <?php } ?>
+
+                                        <?php if ($row['weight']) { ?>
+                                            <div class="form-group">
+                                                <label>Pilih Berat: </label>
+                                                <ul class="selection-input">
+                                                    <?php explodeItem($row, 'weight'); ?>
+                                                </ul>
+                                            </div>
+                                        <?php } ?>
+
+                                        <div class="form-group">
+                                            <label>Kuantitas: </label>
+
+                                            <div class="qty">
+                                                <button type="button" class="min-qty" onclick="this.parentNode.querySelector('#quantity').stepDown()">
+                                                    <i class="fas fa-minus"></i>
+                                                </button>
+                                                <div class="form-outline">
+                                                    <input id="quantity" min="1" max="10" name="quantity" value="1" type="number" class="form-control" readonly />
+                                                </div>
+                                                <button type="button" class="plus-qty" onclick="this.parentNode.querySelector('#quantity').stepUp()">
+                                                    <i class="fas fa-plus"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="region">Wilayah Pengiriman: </label>
+                                            <select class="input-billing form-control" id="region" name="region" required>
+                                                <option value="jabodetabek">Jabodetabek</option>
+                                                <option value="jawa_barat">Jawa Barat</option>
+                                                <option value="jawa_tengah">Jawa Tengah</option>
+                                                <option value="jawa_timur">Jawa Timur</option>
+                                                <option value="luar_jawa">Luar Jawa</option>
+                                                <option value="luar_negeri">Luar Negeri</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer">
+                                        <button class="btn btn-sm btn-beli btn-primary">
+                                            Beli Sekarang
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </article>
+                    </div>
+                </div>
+            </section>
+    <?php }
+    }
+}
+
+if (!$found) { // Jika tidak ditemukan
+    header("Location: ?page=product"); // Redirect ke halaman product
+    exit; // Hentikan script
+}
+
+?>
 
 <section style="padding-top: 30px;">
     <div class="container">
