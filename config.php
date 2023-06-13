009fe5ded23fe8ee7a2e91189bcbc314e83aa8fc
zexpher <fahriangga30@gmail.com>
2023-06-13 14:31:13 +0700

    Update

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ function base_url()
     $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
     $host = $_SERVER['HTTP_HOST'];
     $path = dirname($_SERVER['PHP_SELF']); 
-    $base_url = $protocol . '://' . $host . $path . '/';
+    $base_url = $protocol . '://' . $host . $path;
 
     return $base_url;
 }
