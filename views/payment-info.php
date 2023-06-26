3f848a77efd9f13d1995f974f526d0592fb6f096
zexpher <fahriangga30@gmail.com>
2023-06-27 03:55:55 +0700

    Tambahan kodingan

--- views/payment-info.php
+++ views/payment-info.php
@@ -2,14 +2,14 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header d-flex align-items-center p-2">
-                <b class="modal-title ml-2" id="">Cara Pembelian & Pembayaran di <?= $project_name ?></b>
+                <b class="modal-title ml-2" style="line-height: 1.3;">Cara Pembelian & Pembayaran di <?= $project_name ?></b>
                 <button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close">
                     <i class="fas fa-times"></i>
                 </button>
             </div>
             <div class="modal-body">
                 <ol>
-                    <li>Silahkan pilih produk yang ingin kamu beli.</li>
+                    <li>Silahkan pilih produk yang ingin kamu beli. <a class="underline" href="<?= base_url() ?>?page=product">Klik disini</a></li>
                     <li>Atur pesanan kamu pada di bagian atur pesanan, Lalu klik tombol bayar sekarang. Setelah itu kamu akan diarahkan ke checkout.</li>
                     <li>Pada dihalaman checkout, kamu akan diminta untuk mengisi data diri kamu. Dan masukkan nominal uang kamu (tidak boleh kurang dari total).</li>
                     <li>Setelah itu kamu akan diarahkan ke halaman invoice pembayaran. Selesai!</li>
